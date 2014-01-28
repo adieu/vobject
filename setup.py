ef870dfbb7642756d6b691ebf9f52285ec9e504f
Adieu <adieu@adieu.me>
2014-01-28 23:46:04 +0800

    Bump version number. Fix #3.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ from setuptools import setup, find_packages
 doclines = __doc__.splitlines()
 
 setup(name = "vobject",
-      version = "0.8.1c",
+      version = "0.8.1d",
       author = "Jeffrey Harris",
       author_email = "jeffrey@osafoundation.org",
       license = "Apache",
